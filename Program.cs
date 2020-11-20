44fe56aaaf73194c88e18a7d97581663e66c17f7
ana <aalvesdacostamoura@gmail.com>
2020-11-20 00:52:07 -0300

    inicio do projeto de console

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace projeto_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro numero:")
+            String num1_texto = Console.ReadLine();
+            int num1 = int.Parse(num1_texto);
+
+            Console.WriteLine("digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+          
+            //Console.Write(" o resultado é: ")
+            //console.writeline(soma);
+
+            Console.WriteLine("o resultado de " + num1 + " + " + num2 + " é: " + soma);
+        }
+    }
+}
